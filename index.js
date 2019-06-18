b12b060b1e3bb9ac628a2c8754887f54555f6ef0
Jared Burnham <zlolris97@gmail.com>
2019-06-18 04:05:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 function happyHolidays(){
   return 'Happy holidays!'
 }
+
 function happyHolidaysTo(name){
-  return 'Happy holidays, &{name}!'
+  return 'Happy holidays,'+''+ &{name}+'!'
 }
+
 function holidayCountdown(days, holiday){
   return 'It\'s ${days} days until ${holiday}!'
 }
